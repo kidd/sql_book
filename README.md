347fe918b32464ed8e1aec46b70a6d20d355172d
Raimon Grau <raimonster@gmail.com>
2022-12-04 10:40:09 +0000

    Cleanup seeding process

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sql_book
+
+# Seeding Datasets
+
+To seed the datasets into a postgresql docker container:
+
+- docker-compose up 
+- docker-compose exec -ti db /files/run.sh
--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
-
 set -x
 main() {
-  cd /csv_files/
-  mkdir -p localpath
-  [ -f /localpath ] || ln -s $PWD/localpath/ /localpath
-  cp */* localpath
+  cd /files/ || exit 1
+  rm -fr /files/localpath
+  mkdir -p /files/localpath
+  cp -- */* localpath
+  unlink /localpath ; ln -s "$PWD/localpath/" /localpath
   local creates=(./localpath/create_*)
-  echo psql -h db -p 5432 -U sqluser sqluser "${creates[@]/#/-f }"
-  psql -h db -p 5432 -U sqluser sqluser ${creates[*]/#/-f }
+  psql -h db -p 5432 -U sqluser sqluser ${creates[@]/#/-f }
 }
 
 main "$@"
